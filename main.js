926cf39f3a62aff4f3077b509740156e36a2e08a
devbriana <dev.brianam@gmail.com>
2025-10-04 18:03:31 -0400

    Create basic functions and array container

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
+const myLibrary = [];
+
 function Book (title, author, pages, read) {
+    this.id = crypto.randomUUID();  // unique ID
     this.title = title;
     this.author = author;
     this.pages = pages;
@@ -8,6 +11,16 @@ function Book (title, author, pages, read) {
     }
 }
 
-const theHobbit = new Book("The Hobbit", "by J.R.R. Tolkien", "295 pages", "not read yet");
+function addBookToLibrary(title, author, pages, read) {
+  // take params, create a book then store it in the array
+  const newBook = new Book(title, author, pages, read)
+  myLibrary.push(newBook);
+}
 
-console.log(theHobbit.info());
+
+addBookToLibrary("Goosebumps: Welcome to Dead House", "R.L. Stine", 123, false);
+addBookToLibrary("Junie B. Jones and the Stupid Smelly Bus", "Barbara Park", 96, true);
+addBookToLibrary("Diary of a Wimpy Kid", "Jeff Kinney", 221, false);
+addBookToLibrary("Captain Underpants and the Attack of the Talking Toilets", "Dav Pilkey", 144, true);
+
+console.log(myLibrary);
