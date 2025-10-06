1dab0fa10b5000b6623299db8facb4780d22ec13
devbriana <dev.brianam@gmail.com>
2025-10-06 11:01:08 -0400

    Display Books from Library Array
    - Added a displayBooks() function to loop through the myLibrary array
      and dynamically create a visual card for each book.
    
    - Used JavaScript DOM manipulation to append the generated book cards
      into the HTML #bookshelf container.
    
    - Applied dynamic CSS Grid styling to #bookshelf and added styling
      for .book-card elements.
    
    - Verified functionality with a few manually added books in the array.
    
    Next Step:
    - Implement a “New Book” button that opens a form for user input
      to add new book information to the library.

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+const newBookButton = document.getElementById('new-book-button')
+
 const myLibrary = [];
 
 function Book (title, author, pages, read) {
@@ -22,5 +24,37 @@ addBookToLibrary("Goosebumps: Welcome to Dead House", "R.L. Stine", 123, false);
 addBookToLibrary("Junie B. Jones and the Stupid Smelly Bus", "Barbara Park", 96, true);
 addBookToLibrary("Diary of a Wimpy Kid", "Jeff Kinney", 221, false);
 addBookToLibrary("Captain Underpants and the Attack of the Talking Toilets", "Dav Pilkey", 144, true);
+addBookToLibrary("Goosebumps: Welcome to Dead House", "R.L. Stine", 123, false);
+addBookToLibrary("Junie B. Jones and the Stupid Smelly Bus", "Barbara Park", 96, true);
+addBookToLibrary("Diary of a Wimpy Kid", "Jeff Kinney", 221, false);
+addBookToLibrary("Captain Underpants and the Attack of the Talking Toilets", "Dav Pilkey", 144, true);
 
 console.log(myLibrary);
+
+function displayBooks() {
+  const bookshelf = document.getElementById("bookshelf");
+ 
+  myLibrary.forEach((book) => {
+    const cardDiv = document.createElement("div");
+    cardDiv.classList.add("book-card")
+
+  cardDiv.innerHTML = `
+    <h3>${book.title}</h3>
+    <p><strong>Author:</strong> ${book.author}</p>
+    <p><strong>Pages Read:</strong> ${book.pages}</p>
+    <p><strong>Read:</strong> ${book.read ? "Yes" : "No"}</p>
+  `
+
+  bookshelf.appendChild(cardDiv);
+  })
+}
+
+displayBooks();
+
+// New book button functionality
+
+newBookButton.addEventListener("click", displayForm())
+
+function displayForm() {
+  
+}
